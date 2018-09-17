adb9beee63ed02a1c8ba64f219d81ab9c8f06348
Theodore Dubois <tblodt@icloud.com>
2018-09-17 11:06:47 -0700

    Convert tabs to spaces

--- site.js
+++ site.js
@@ -1,27 +1,27 @@
 jQuery.easing.def = "easeInOutQuad";
 
 $(window).on("load", function() {
-	let body = $(document.body);
-	let root = $("html, body");
+    let body = $(document.body);
+    let root = $("html, body");
 
-	$("a[href^=\"#\"]").click(function(e) {
-		let href = $.attr(this, "href");
-		if (href == "#") {
-			return;
-		}
-		e.preventDefault();
+    $("a[href^=\"#\"]").click(function(e) {
+        let href = $.attr(this, "href");
+        if (href == "#") {
+            return;
+        }
+        e.preventDefault();
 
-		root.animate({
-			scrollTop: $(href).offset().top - 80 - 11
-		}, "slow");
-	});
+        root.animate({
+            scrollTop: $(href).offset().top - 80 - 11
+        }, "slow");
+    });
 });
 
 $(window).scroll(function() {
-	let body = $(document.body);
-	let header = $("header");
-	let y = $(window).scrollTop();
+    let body = $(document.body);
+    let header = $("header");
+    let y = $(window).scrollTop();
 
-	header.css("background", "rgba(38, 38, 38, " + Math.min(1, y * (1 / 200)) + ")");
-	header.css("box-shadow", "0 0 " + Math.min(20, (y * (20 / 200))) + "px black");
+    header.css("background", "rgba(38, 38, 38, " + Math.min(1, y * (1 / 200)) + ")");
+    header.css("box-shadow", "0 0 " + Math.min(20, (y * (20 / 200))) + "px black");
 });

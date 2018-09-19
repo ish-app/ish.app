787dc1f1da1f75c01a6a2a1edd69fe5f933a1e46
MatthewMerrill <MattMerr47@gmail.com>
2018-09-19 11:32:21 -0700

    something very important

--- site.js
+++ site.js
@@ -15,6 +15,22 @@ $(window).on("load", function() {
             scrollTop: $(href).offset().top - 80
         }, "slow");
     });
+
+    let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+    let nextKonamiInput = 0;
+    $(window).keydown(function(ev) {
+        if (nextKonamiInput !== -1 && ev.keyCode === konamiCode[nextKonamiInput]) {
+            nextKonamiInput += 1;
+            if (nextKonamiInput === konamiCode.length) {
+                $('.home').css('background', 'url(https://pbs.twimg.com/profile_banners/2540435814/1537379284/1500x500)').css('background-size', 'cover');
+                nextKonamiInput = -1;
+            }
+        } else {
+            if (nextKonamiInput !== 3 || ev.keyCode !== 38) {
+                nextKonamiInput = 0;
+            }
+        }
+    });
 });
 
 $(window).scroll(function() {

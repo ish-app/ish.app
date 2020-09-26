383c75d58ed6a8c15253f2f7089bb8d5e9a02929
Martin Persson <mnpn03@icloud.com>
2020-09-27 01:49:35 +0200

    Redesigned header

--- site.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function(e) {
-    let header = document.getElementById("headr");
-    let y = document.documentElement.scrollTop;
-
-    header.style.background = "rgba(38, 38, 38, " + Math.min(1, y * (1 / 200)) + ")";
-    header.style.boxShadow = "0 0 " + Math.min(20, (y * (20 / 200))) + "px black";
-});
